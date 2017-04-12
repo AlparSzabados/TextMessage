475da70c9a463e3834b4436b3c90445bfa4786ea
Szabados Alpár <szabadosalpar@gmail.com>
2017-04-12 22:35:10 +0300

    setup commit

---
+++ app/src/main/java/textmessage/szabados/alpar/textmessage/EmergencyText.java
@@ -0,0 +1,16 @@
+package textmessage.szabados.alpar.textmessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
+
+public class EmergencyText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_text);
+    }
+}
