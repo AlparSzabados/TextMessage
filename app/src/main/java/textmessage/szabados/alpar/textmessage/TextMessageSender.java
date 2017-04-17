6d8ad5fdf2975c3ff326132d4000628b0b54c105
Szabados Alpár <szabadosalpar@gmail.com>
2017-04-17 20:45:28 +0300

    Added Toasts for sent and unsent messages

--- app/src/main/java/textmessage/szabados/alpar/textmessage/TextMessageSender.java
+++ app/src/main/java/textmessage/szabados/alpar/textmessage/TextMessageSender.java
@@ -6,10 +6,13 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class TextMessageSender extends AppCompatActivity {
     public static final SmsManager SMS_MANAGER = SmsManager.getDefault();
-    public static final String DESTINATION_ADDRESS = "";
+    public static final String DESTINATION_ADDRESS = "6505551212";
 
     private EditText customText;
 
@@ -21,14 +24,24 @@ public class TextMessageSender extends AppCompatActivity {
         customText = (EditText) findViewById(R.id.customText);
     }
 
-    public void onSend(View view) {
+    public void onClick(View view) {
         sendMessage(view);
         clearMessage();
     }
 
     private void sendMessage(View view) {
         String message = getMessage(view);
-        SMS_MANAGER.sendTextMessage(DESTINATION_ADDRESS, null, message, null, null);
+
+        if (!Objects.equals(message, "")) {
+            SMS_MANAGER.sendTextMessage(DESTINATION_ADDRESS, null, message, null, null);
+            getToast("Message Sent!").show();
+        } else {
+            getToast("No Message to Send!").show();
+        }
+    }
+
+    private Toast getToast(String text) {
+        return Toast.makeText(this, text, Toast.LENGTH_SHORT);
     }
 
     private void clearMessage() {
@@ -37,9 +50,6 @@ public class TextMessageSender extends AppCompatActivity {
 
     private String getMessage(View view) {
         if (view.getId() == R.id.fab) return customText.getText().toString();
-        else {
-            Button button = (Button) view;
-            return button.getText().toString();
-        }
+        else return ((Button) view).getText().toString();
     }
 }
