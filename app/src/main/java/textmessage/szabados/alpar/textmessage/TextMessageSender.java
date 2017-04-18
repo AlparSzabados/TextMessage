1506cd899d372edac2335e48512bdb4606b0f8ea
Szabados Alpár <szabadosalpar@gmail.com>
2017-04-18 12:07:45 +0300

    Added phone number pick logic

--- app/src/main/java/textmessage/szabados/alpar/textmessage/TextMessageSender.java
+++ app/src/main/java/textmessage/szabados/alpar/textmessage/TextMessageSender.java
@@ -1,6 +1,11 @@
 package textmessage.szabados.alpar.textmessage;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -12,8 +17,8 @@ import java.util.Objects;
 
 public class TextMessageSender extends AppCompatActivity {
     public static final SmsManager SMS_MANAGER = SmsManager.getDefault();
-    public static final String DESTINATION_ADDRESS = "6505551212";
-
+    private static final int CONTACT_PICKER_RESULT = 1;
+    public EditText destinationAddress;
     private EditText customText;
 
     @Override
@@ -22,6 +27,7 @@ public class TextMessageSender extends AppCompatActivity {
         setContentView(R.layout.activity_text_message_sender);
 
         customText = (EditText) findViewById(R.id.customText);
+        destinationAddress = (EditText) findViewById(R.id.phone_number);
     }
 
     public void onClick(View view) {
@@ -31,9 +37,10 @@ public class TextMessageSender extends AppCompatActivity {
 
     private void sendMessage(View view) {
         String message = getMessage(view);
+        String destinationAddress = this.destinationAddress.getText().toString();
 
         if (!Objects.equals(message, "")) {
-            SMS_MANAGER.sendTextMessage(DESTINATION_ADDRESS, null, message, null, null);
+            SMS_MANAGER.sendTextMessage(destinationAddress, null, message, null, null);
             getToast("Message Sent!").show();
         } else {
             getToast("No Message to Send!").show();
@@ -51,5 +58,40 @@ public class TextMessageSender extends AppCompatActivity {
     private String getMessage(View view) {
         if (view.getId() == R.id.fab) return customText.getText().toString();
         else return ((Button) view).getText().toString();
+    }
+
+    public void pickPhoneNumber(View view) {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) pickContactPhoneNumber(data);
+    }
+
+    private void pickContactPhoneNumber(Intent data) {
+        ContentResolver contentResolver = getContentResolver();
+        try {
+            Uri uri = data.getData();
+            Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+            assert cursor != null;
+            cursor.moveToFirst();
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String contactId = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?";
+            Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+            Cursor phoneCursor = contentResolver.query(phoneUri, null, contactId, new String[]{id}, null);
+
+            assert phoneCursor != null;
+            while (phoneCursor.moveToNext()) {
+                String phone = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                destinationAddress.setText(phone);
+            }
+            cursor.close();
+            phoneCursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
